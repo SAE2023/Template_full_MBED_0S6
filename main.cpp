c9fbfe795c33e632736ac99267d4f66a0adcd9be
alzina9166 <gilles.raynaud1@gmail.com>
2022-11-07 08:13:10 +0000

    OK1

--- main.cpp
+++ main.cpp
@@ -10,14 +10,16 @@ DigitalOut led1(LED1);
 
 int main()
 {
-    double xx=1.12345;
+    double pi=1.12345;
     printf("Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-    xx=4*atan(1.0);
+    pi=4*atan(1.0);
+    printf("Print float %f\n", pi);
+    pi=0;
     while (true)
     {
         led1 = !led1;
-        printf("Print float %f\n", xx);
-        xx=xx+1;
+        printf("Print float %f\n", pi);
+        pi=pi+1;
         thread_sleep_for(WAIT_TIME_MS);
     }
 }
