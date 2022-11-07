20fb1497d0ed9b60e747f0ea3c25553c6105c90c
SAE GEII Cachan <gilles.raynaud1@gmail.com>
2022-11-07 10:32:48 +0000

    OK2

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@ int main()
     double pi=0;
     printf("Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
     pi=4*atan(1.0);
-    printf("Print Pi %f\n", pi);
+    printf("Print Pi = %f\n", pi);
     pi=0;
     while (true)
     {
         led1 = !led1;
-        printf("Print float %f\n", pi);
+        printf("Print float counter = %f\n", pi);
         pi=pi+1;
         thread_sleep_for(WAIT_TIME_MS);
     }
