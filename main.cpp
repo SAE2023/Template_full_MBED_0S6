80d00dbe76388b609fd708a6c9273b11da808e21
alzina9166 <117350151+alzina9166@users.noreply.github.com>
2022-11-07 09:06:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,10 +12,12 @@ int main()
 {
     double xx=1.12345;
     printf("Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-    printf("Print float %f\n", 4*atan(1.0));
+    xx=4*atan(1.0);
     while (true)
     {
         led1 = !led1;
+        printf("Print float %f\n", xx);
+        xx=xx+1;
         thread_sleep_for(WAIT_TIME_MS);
     }
 }
